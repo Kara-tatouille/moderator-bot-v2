011d363f8581603e1c5e816b42fd114987943add
Kara Richard-Vitton <corenbla@gmail.com>
2022-01-28 16:20:26 +0100

    event files

---
+++ events/interactionCreate.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+
+        // Try to run a command from the interaction
+        if (!interaction.isCommand()) {
+            return;
+        }
+
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) {
+            return;
+        }
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
+        }
+
+    },
+};
--- index.js
+++ index.js
@@ -1,41 +1,27 @@
-// Require the necessary discord.js classes
 const {Client, Intents, Collection} = require('discord.js');
 const {token} = require('./config/config.json');
 const fs = require('fs');
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
-client.once('ready', () => {
-    console.log('Ready!');
-});
-
-// Create collection of commands from files
+// Create collection of commands in ./commands
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    // Set a new item in the Collection
-    // With the key as the command name and the value as the exported module
     client.commands.set(command.data.name, command);
 }
 
-// Try to run commands on interaction reception
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-    /** @var CommandInteraction<Cached> interaction **/
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// Register events from files in ./events
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-});
+}
 
 // Login to Discord with your client's token
 client.login(token);
