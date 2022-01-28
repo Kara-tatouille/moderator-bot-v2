b1ac77710ab7d9b6835a3a9eec3792fd205e53ee
Kara Richard-Vitton <corenbla@gmail.com>
2022-01-28 16:06:34 +0100

    command files

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 // Require the necessary discord.js classes
-const {Client, Intents} = require('discord.js');
+const {Client, Intents, Collection} = require('discord.js');
 const {token} = require('./config/config.json');
+const fs = require('fs');
 
-// Create a new client instance
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
-// When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
 });
 
+// Create collection of commands from files
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
+
+// Try to run commands on interaction reception
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) {
-        return;
-    }
+    if (!interaction.isCommand()) return;
+    /** @var CommandInteraction<Cached> interaction **/
 
-    const {commandName} = interaction;
+    const command = client.commands.get(interaction.commandName);
 
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    } else if (commandName === 'server') {
-        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-    } else if (commandName === 'user') {
-        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
 
