3132b3f23b287faaf0dc1a4717836a7f6c1cc372
Kara Richard-Vitton <corenbla@gmail.com>
2022-04-09 18:05:01 +0200

    added language option to the maintenance command

--- src/commands/maintenance.ts
+++ src/commands/maintenance.ts
@@ -1,4 +1,5 @@
 import {CommandInteraction} from "discord.js";
+import {SlashCommandStringOption} from "@discordjs/builders";
 
 const {SlashCommandBuilder} = require('@discordjs/builders');
 const {trans} = require('../services/translate');
@@ -6,8 +7,17 @@ const {trans} = require('../services/translate');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('maintenance')
-        .setDescription('Sends a (translated) message explaining the weekly maintenance'),
+        .setDescription('Sends a (translated) message explaining the weekly maintenance')
+        .addStringOption((option: SlashCommandStringOption) => {
+            return option
+                .setName('language')
+                .setDescription('Language of the message')
+                .setRequired(true)
+                .addChoice('fr', 'fr')
+                .addChoice('en', 'en')
+        }),
     async execute(interaction: CommandInteraction): Promise<void> {
-        await interaction.reply(trans('maintenance', interaction.locale));
+        const locale = interaction.options.getString('language') ?? 'fr';
+        await interaction.reply(trans('maintenance', locale));
     },
 };
