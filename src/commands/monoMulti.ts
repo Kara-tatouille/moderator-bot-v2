5e2382f736f0512da5e2bcb01d61132e8c03ef1e
Portevent <74187611+Portevent@users.noreply.github.com>
2022-04-21 16:47:02 +0200

    Update Mono vs Multi tooltip
    Switch from plain text to embeds

--- src/commands/monoMulti.ts
+++ src/commands/monoMulti.ts
@@ -18,6 +18,23 @@ module.exports = {
         }),
     async execute(interaction: CommandInteraction): Promise<void> {
         const locale = interaction.options.getString('language') ?? 'fr';
-        await interaction.reply(trans('monomulti', locale));
+        
+        const mainEmbed = new MessageEmbed()
+            .setDescription(trans("tooltip.monomulti.main.description")
+            .setTitle(trans("tooltip.monomulti.main.title"))
+                            
+        const multiEmbed = new MessageEmbed()
+            .setDescription(trans("tooltip.monomulti.multi.description")
+            .setAuthor(trans("tooltip.monomulti.multi.title"))
+                            
+        const monoEmbed = new MessageEmbed()
+            .setDescription(trans("tooltip.monomulti.mono.description")
+            .setAuthor(trans("tooltip.monomulti.mono.title"))
+            .setFields(monoProsCons)
+                            
+        for(int i = 0; i < 4; i++) multiEmbed = multiEmbed.addField(trans("tooltip.monomulti.multi.fieldTitle" + i), trans("tooltip.monomulti.multi.field" + i"))                  
+        for(int i = 0; i < 4; i++) multiEmbed = multiEmbed.addField(trans("tooltip.monomulti.mono.fieldTitle" + i), trans("tooltip.monomulti.mono.field" + i"))
+        
+        await interaction.reply({embeds: [mainEmbed, multiEmbed, monoEmbed]});
     },
 };
