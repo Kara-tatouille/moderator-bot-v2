a4c1f3d90e18b25a71d3bbc0be1b1bee96dc9bde
Portevent <74187611+Portevent@users.noreply.github.com>
2022-04-22 14:08:38 +0200

    Update MonoMulti
    Delegate the creation of the embed to the tooltipView service

--- src/commands/monoMulti.ts
+++ src/commands/monoMulti.ts
@@ -3,6 +3,7 @@ import {SlashCommandStringOption} from "@discordjs/builders"
 
 const {SlashCommandBuilder} = require('@discordjs/builders');
 const {trans} = require('../services/translate');
+const {getMonoMulti} = require('../services/tooltipView');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -16,25 +17,7 @@ module.exports = {
                 .addChoice('fr', 'fr')
                 .addChoice('en', 'en')
         }),
-    async execute(interaction: CommandInteraction): Promise<void> {
-        const locale = interaction.options.getString('language') ?? 'fr';
-        
-        const mainEmbed = new MessageEmbed()
-            .setDescription(trans("tooltip.monomulti.main.description")
-            .setTitle(trans("tooltip.monomulti.main.title"))
-                            
-        const multiEmbed = new MessageEmbed()
-            .setDescription(trans("tooltip.monomulti.multi.description")
-            .setAuthor(trans("tooltip.monomulti.multi.title"))
-                            
-        const monoEmbed = new MessageEmbed()
-            .setDescription(trans("tooltip.monomulti.mono.description")
-            .setAuthor(trans("tooltip.monomulti.mono.title"))
-            .setFields(monoProsCons)
-                            
-        for(int i = 0; i < 4; i++) multiEmbed = multiEmbed.addField(trans("tooltip.monomulti.multi.fieldTitle" + i), trans("tooltip.monomulti.multi.field" + i"))                  
-        for(int i = 0; i < 4; i++) multiEmbed = multiEmbed.addField(trans("tooltip.monomulti.mono.fieldTitle" + i), trans("tooltip.monomulti.mono.field" + i"))
-        
-        await interaction.reply({embeds: [mainEmbed, multiEmbed, monoEmbed]});
+    async execute(interaction: CommandInteraction): Promise<void> {        
+        await interaction.reply({embeds: getMonoMulti(interaction.options.getString('language') ?? 'fr')});
     },
 };
